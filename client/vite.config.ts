c31039f34346c4d5950a4717f421c170fa64318e
Deepak Maurya <deepakmaurya@Deepaks-MacBook-Air.local>
2025-04-26 15:10:26 +0530

    server deploy 1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
     port: 4173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://we-doodle-server.vercel.app/',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:3000',
+        target: 'https://we-doodle-server.vercel.app/',
         changeOrigin: true,
         ws: true,
       },
