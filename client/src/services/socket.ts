fc76f1fe6484892483c143c84b611f924f9c13f7
Deepak Maurya <deepakmaurya@Deepaks-MacBook-Air.local>
2025-04-26 15:13:35 +0530

    server deploy 2

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 import { DrawingData } from '../types'
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const SOCKET_URL = 'https://we-doodle-server.vercel.app/'
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
